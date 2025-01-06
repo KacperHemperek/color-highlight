70dc5c025b809322c997d088f6b457ac10800f26
Kacper Hemperek <kacper.hemperek@apptimia.com>
2025-01-06 17:31:32 +0100

    reafactor: move editor and extension to separate files

---
+++ src/components/editor.tsx
@@ -0,0 +1,29 @@
+import { EditorContent, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import { ColorsPlugin } from "../lib/color-extension";
+import Placeholder from "@tiptap/extension-placeholder";
+
+export function Editor() {
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      ColorsPlugin,
+      Placeholder.configure({
+        placeholder: "Paste or type your text here...",
+        showOnlyCurrent: true,
+        emptyEditorClass:
+          "text-slate-700 before:content-[attr(data-placeholder)]",
+      }),
+    ],
+    editorProps: {
+      attributes: {
+        class:
+          "min-h-96 p-2 border border-slate-700 rounded-md font-mono outline-none focus:ring focus:ring-sky-500",
+      },
+    },
+  });
+
+  if (!editor) return null;
+
+  return <EditorContent editor={editor} />;
+}
